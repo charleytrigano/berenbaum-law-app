21cae08dbedbf78cc6dec3e3fcd7b35644e9345e
charleytrigano <charley@trigano.org>
2025-11-25 21:47:15 +0100

    Integrate Streamlit to show accounting data
    Added Streamlit integration to display accounting data.

--- 📁 pages/08_📒_Comptabilite.py
+++ 📁 pages/08_📒_Comptabilite.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from backend.google_sheets import load_sheet
+from utils.config import SHEET_COMPTA
+
+st.title("📒 Comptabilité")
+
+df = load_sheet(SHEET_COMPTA)
+st.dataframe(df, use_container_width=True)
 
