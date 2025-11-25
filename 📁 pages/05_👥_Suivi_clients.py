28fba7265829cf7f89624b4dadf1c4c0994d0982
charleytrigano <charley@trigano.org>
2025-11-25 21:43:22 +0100

    Add client tracking page with data display

---
+++ 📁 pages/05_👥_Suivi_clients.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from backend.google_sheets import load_sheet
+from utils.config import SHEET_CLIENTS
+
+st.title("👥 Suivi clients")
+
+df = load_sheet(SHEET_CLIENTS)
+
+st.dataframe(df[["Dossier N", "Nom"]], use_container_width=True)
