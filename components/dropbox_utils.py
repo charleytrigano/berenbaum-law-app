2a38e002749eca7567b7f416cb9a6b4e73f1c86a
charleytrigano <charley@trigano.org>
2025-11-26 17:42:21 +0100

    Refactor Dropbox utility functions for clarity

--- components/dropbox_utils.py
+++ components/dropbox_utils.py
@@ -1,24 +1,23 @@
 import dropbox
 import streamlit as st
 
-# Connexion à Dropbox
 def get_dbx():
     return dropbox.Dropbox(st.secrets["DROPBOX_TOKEN"])
 
-# Upload
-def upload_to_dropbox(file, dropbox_path):
+def upload_bytes_to_dropbox(content: bytes, dropbox_path: str):
+    dbx = get_dbx()
+    dbx.files_upload(content, dropbox_path, mode=dropbox.files.WriteMode("overwrite"))
+
+def upload_file_to_dropbox(file, dropbox_path):    
     dbx = get_dbx()
     dbx.files_upload(file.read(), dropbox_path, mode=dropbox.files.WriteMode("overwrite"))
-    return True
 
-# Download
 def download_from_dropbox(dropbox_path):
     dbx = get_dbx()
-    meta, res = dbx.files_download(dropbox_path)
+    _, res = dbx.files_download(dropbox_path)
     return res.content
 
-# List files
-def list_files(folder="/"):
+def list_folder(dropbox_path="/"):
     dbx = get_dbx()
-    items = dbx.files_list_folder(folder).entries
+    items = dbx.files_list_folder(dropbox_path).entries
     return [item.name for item in items]
