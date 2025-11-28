05c077d43b1710286d8c8b4d04f06a7370b49694
charleytrigano <charley@trigano.org>
2025-11-28 13:09:56 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,10 @@
 FILE_ID = "1HU8rfXaG_uu-TVcAEtbs00jsvpQaiAgP"
+import streamlit as st
 
+DROPBOX_TOKEN = st.secrets["DROPBOX_TOKEN"]
+DROPBOX_FILE_PATH = "/database.json"  # ton fichier est bien à la racine
 SHEET_CLIENTS = "Clients"
 SHEET_ESCROW = "Escrow"
 SHEET_VISA = "Visa"
 SHEET_COMPTA = "ComptaCli"
-DROPBOX_FILE_PATH = "/database.json"
 
