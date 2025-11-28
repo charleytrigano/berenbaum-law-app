be8bb9c85601308029450c197d043fe6ead98e2c
charleytrigano <charley@trigano.org>
2025-11-28 16:17:22 +0100

    Update 99_convert_excel.py

--- pages/99_convert_excel.py
+++ pages/99_convert_excel.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from backend.convert_excel_to_json import convert_excel_to_json
+
+st.title("🛠️ Conversion Excel → JSON")
+
+if st.button("Convertir maintenant"):
+    convert_excel_to_json()
+    st.success("Base JSON générée avec succès depuis l’Excel !")
 
