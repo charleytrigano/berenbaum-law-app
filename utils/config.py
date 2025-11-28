17c55a14f51b6db22347bf30c1d8394a7dde47f4
charleytrigano <charley@trigano.org>
2025-11-28 19:32:45 +0100

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
-# ---- Dropbox config ----
 DROPBOX_TOKEN = st.secrets["dropbox"]["DROPBOX_TOKEN"]
 DROPBOX_FILE_PATH = st.secrets["paths"]["DROPBOX_FILE_PATH"]
 
-# ---- Sheet Names ----
 SHEET_CLIENTS = "Clients"
 SHEET_ESCROW = "Escrow"
 SHEET_VISA = "Visa"
