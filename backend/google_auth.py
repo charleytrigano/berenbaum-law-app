e89006befd6792222cc0455f7f560a69556690d0
charleytrigano <charley@trigano.org>
2025-11-28 13:24:46 +0100

    Delete backend directory

--- backend/google_auth.py
+++
@@ -1,7 +0,0 @@
-def get_gsheet_service():
-    """
-    Google Sheets n'est plus utilisé dans cette application.
-    Cette fonction est conservée uniquement pour compatibilité
-    afin d'éviter l'erreur d'import dans les modules existants.
-    """
-    raise Exception("Google Sheets n'est plus supporté dans cette application.")
