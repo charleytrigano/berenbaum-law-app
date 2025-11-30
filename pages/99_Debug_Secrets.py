480a31eec646e374e6c62d324b9a995e128df124
charleytrigano <charley@trigano.org>
2025-11-30 14:02:42 +0100

    Add configuration for Dropbox secrets and paths

---
+++ pages/99_Debug_Secrets.py
@@ -0,0 +1,11 @@
+{
+  "dropbox": {
+    "APP_KEY": "dec5zj63pkh1clf",
+    "APP_SECRET": "..."
+    "DROPBOX_TOKEN": "..."
+  },
+  "paths": {
+      "CLIENTS_FILE": "/Apps/Clients.xlsx",
+      ...
+  }
+}
