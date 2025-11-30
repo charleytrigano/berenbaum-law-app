0cf4bd93a7224319ef3956a310cf4e0a6bd5ed9d
charleytrigano <charley@trigano.org>
2025-11-30 20:22:00 +0100

    Add dependencies dictionary for immigration categories

---
+++ utils/dependencies.py
@@ -0,0 +1,44 @@
+dependencies = {
+
+    "Affaires / Tourisme": {
+        "B-1": ["1-COS", "2-EOS"],
+        "B-2": ["1-COS", "2-EOS"]
+    },
+
+    "Professionnel": {
+        "P-1": ["1-Inv.", "2-CP", "3-USCIS"],
+        "P-2": ["1-CP", "2-USCIS"]
+    },
+
+    "Travailleur temporaire": {
+        "H-1B": ["1-Initial", "2-Extension", "3-Transfer", "4-CP"],
+        "H-2B": ["2-CP.1"],
+        "E-3": ["1-Employement"]
+    },
+
+    "Immigration permanente - EB": {
+        "EB1": ["1-I-140", "2-AOS", "3-I-140 & AOS", "4-CP.1"],
+        "EB2": ["4-Perm", "5-CP"],
+        "EB5": ["1-I-526", "2-AOS.1", "3-I527 & AOS", "4-CP.2", "I-829"]
+    },
+
+    "Immigration familiale": {
+        "I-130": ["2-I-130", "3-AOS", "4-I-130 & AOS", "5-CP.1"],
+        "K-1": ["1-CP.1", "2-AOS.2"]
+    },
+
+    "Autres": {
+        "Traditional": ["Traditional"],
+        "Marriage": ["Marriage"],
+        "Derivatives": ["Derivatives"],
+        "Travel Permit": ["Travel Permit"],
+        "Work Permit": ["Work Permit"],
+        "I-751": ["I-751"],
+        "Re-entry Permit": ["Re-entry Permit"],
+        "I-90": ["I-90"],
+        "Consultation": ["Consultation"],
+        "Analysis": ["Analysis"],
+        "Referral": ["Referral"],
+        "I-407": ["I-407"]
+    }
+}
