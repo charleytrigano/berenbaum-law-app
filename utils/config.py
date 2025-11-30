533a6fd4550f303653372bd1793a6ed3c180f8b1
charleytrigano <charley@trigano.org>
2025-11-30 19:58:49 +0100

    Update Dropbox configuration and file paths

--- utils/config.py
+++ utils/config.py
@@ -1,13 +1,17 @@
 import streamlit as st
 
-# ---- Dropbox config ----
-DROPBOX_TOKEN = st.secrets["dropbox"]["DROPBOX_TOKEN"]
+# --------------------------------------------------
+# 🔐 DROBOX AUTH
+# --------------------------------------------------
+APP_KEY = st.secrets["dropbox"]["APP_KEY"]
+APP_SECRET = st.secrets["dropbox"]["APP_SECRET"]
+DROPBOX_REFRESH_TOKEN = st.secrets["dropbox"]["DROPBOX_TOKEN"]  # refresh token Dropbox
 
-# 🔥 Chemin correct du fichier JSON dans Dropbox :
-DROPBOX_FILE_PATH = "/Apps/berenbaum-law/database.json"
-
-# ---- Sheet names (non utilisés mais conservés) ----
-SHEET_CLIENTS = "Clients"
-SHEET_ESCROW = "Escrow"
-SHEET_VISA = "Visa"
-SHEET_COMPTA = "ComptaCli"
+# --------------------------------------------------
+# 📁 PATHS DROPBOX
+# --------------------------------------------------
+CLIENTS_FILE = st.secrets["paths"]["CLIENTS_FILE"]
+ESCROW_FILE = st.secrets["paths"]["ESCROW_FILE"]
+VISA_FILE = st.secrets["paths"]["VISA_FILE"]
+COMPTA_FILE = st.secrets["paths"]["COMPTA_FILE"]
+DROPBOX_JSON = st.secrets["paths"]["DROPBOX_JSON"]
