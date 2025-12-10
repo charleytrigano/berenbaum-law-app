32f5f0d39b60d71d5c840f328991d609ce063bbd
charleytrigano <charley@trigano.org>
2025-12-10 11:55:23 +0100

    Import and call render_sidebar function

--- pages/02_➕_Nouveau_dossier.py
+++ pages/02_➕_Nouveau_dossier.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from utils.sidebar import render_sidebar
+render_sidebar()
 import pandas as pd
 from backend.dropbox_utils import load_database, save_database
 
-from utils.sidebar import render_sidebar
-render_sidebar()
+
 
 
 st.set_page_config(page_title="Nouveau dossier", page_icon="➕", layout="wide")
