c8d057f6e63f957c1454c8d50c39cdbe25293187
charleytrigano <charley@trigano.org>
2025-12-10 11:35:02 +0100

    Add sidebar rendering to Liste_dossiers page

--- pages/01_📁_Liste_dossiers.py
+++ pages/01_📁_Liste_dossiers.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 from backend.dropbox_utils import load_database
 
+from utils.sidebar import render_sidebar
+render_sidebar()
+
+
 st.set_page_config(page_title="Liste des dossiers", page_icon="📁", layout="wide")
 st.title("📁 Liste des dossiers")
 
