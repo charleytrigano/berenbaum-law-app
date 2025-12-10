ee66a7a9c06fbc6c020268bfb3d1f48e2ed0bd12
charleytrigano <charley@trigano.org>
2025-12-10 11:57:15 +0100

    Remove duplicate sidebar rendering
    Removed duplicate sidebar rendering in Visa.py.

--- pages/07_🛂_Visa.py
+++ pages/07_🛂_Visa.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from utils.sidebar import render_sidebar
+render_sidebar()
 import pandas as pd
 from backend.dropbox_utils import load_database, save_database
 
-from utils.sidebar import render_sidebar
-render_sidebar()
+
 
 st.set_page_config(page_title="Visa – Paramétrage", page_icon="🛂", layout="wide")
 st.title("🛂 Paramétrage des categories, sous-categories et visas")
